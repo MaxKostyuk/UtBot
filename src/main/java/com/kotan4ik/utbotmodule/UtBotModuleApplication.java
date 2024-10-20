e88d7c3a74e4e9c94772afa4d3838829cd874f9f
kotan4ik <kostyuk.max@gmail.com>
2024-10-20 17:39:20 +0800

    feat: Spring module UtBotModule added

---
+++ src/main/java/com/kotan4ik/utbotmodule/UtBotModuleApplication.java
@@ -0,0 +1,13 @@
+package com.kotan4ik.utbotmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtBotModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtBotModuleApplication.class, args);
+    }
+
+}
