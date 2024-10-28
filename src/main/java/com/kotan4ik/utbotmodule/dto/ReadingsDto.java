71acb3cf1c8939c5a7ab8027b2fc4152bb43652d
kotan4ik <kostyuk.max@gmail.com>
2024-10-28 17:35:41 +0800

    feat: new dto ReadingsDto added

---
+++ src/main/java/com/kotan4ik/utbotmodule/dto/ReadingsDto.java
@@ -0,0 +1,14 @@
+package com.kotan4ik.utbotmodule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReadingsDto {
+    private int t1;
+    private int t2;
+    private int t3;
+    private int cold;
+    private int hot;
+}
